72ce897f71d45785397dde765d86ce78fdacf5d9
louis-ngugi <louisngugi858@gmail.com>
2025-01-13 07:54:14 +0300

    created codes in index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ name, email }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to save data');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Append the new ID to the DOM
+      document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+    })
+    .catch((error) => {
+      // Append the error message to the DOM
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
+
